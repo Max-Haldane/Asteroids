65fe834d4eedc607e4c6ef940fccc21168695064
Max-Haldane <maximehaldane@gmail.com>
2026-09-03 01:04:00 +0800

    C: Added framerate of 60 fps

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from constants import SCREEN_HEIGHT
 from logger import log_state
 
 def main():
-	pygame.init()
+
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0.0
+	
 	while True:
 		log_state()
 		for event in pygame.event.get():
@@ -16,6 +21,7 @@ def main():
 				return
 		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
