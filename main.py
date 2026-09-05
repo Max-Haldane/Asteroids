8bd555c71fdb548d694b4e0a926f82a41978734b
Max-Haldane <maximehaldane@gmail.com>
2026-09-05 08:40:53 +0800

    G: Incorporated updateable and drawable groups

--- main.py
+++ main.py
@@ -14,16 +14,24 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0.0
+
+	updateable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updateable, drawable)
+
 	player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-	
+
 	while True:
 		log_state()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
-		player.update(dt)
-		player.draw(screen)
+		for obj in updateable:
+			obj.update(dt)
+		for obj in drawable:
+			obj.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
